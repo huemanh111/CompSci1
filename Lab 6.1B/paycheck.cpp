4671d8832403a7be89dc0b2135a9d9e491e120ab
Hue Anh <huynh.hueanh1@gmail.com>
2018-02-26 22:39:40 -0500

    Add files via upload

---
+++ Lab 6.1B/paycheck.cpp
@@ -0,0 +1,118 @@
+// This program takes two numbers (payRate & hours)
+// and multiplies them to get grosspay.
+// It then calculates net pay by subtracting 15%
+
+//Hue Anh Huynh
+//3/12/17
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+// Function prototypes
+void printDescription();
+void computePaycheck(float, int, float, float);
+
+int main()
+{
+	float payRate;
+	float grossPay;
+	float netPay;
+	int hours;
+
+	cout << setprecision(2) << fixed;
+	cout << "Welcome to the Pay Roll Program" << endl;
+
+	printDescription();		// Call to Description function
+
+	cout << "Please input the pay per hour" << endl;
+	cin >> payRate;
+
+	cout << endl << "Please input the number of hours worked" << endl;
+	cin >> hours;
+
+	cout << endl << endl;
+
+	computePaycheck(payRate, hours, grossPay, netPay);
+
+	// Fill in the code to output grossPay
+//  cout << "Gross Pay: $" << grossPay << endl;
+//    cout << "Net Pay: $" << netPay << endl;
+
+	cout << "We hope you enjoyed this program" << endl;
+
+	return 0;
+}
+
+//	********************************************************************
+//	printDescription
+//
+//	task:	  This function prints a program description
+//	data in:  none
+//	data out: no actual parameter altered
+//
+//	********************************************************************
+
+void printDescription() // The function heading
+{
+	cout << "************************************************" << endl << endl;
+	cout << "This program takes two numbers (payRate & hours)" << endl;
+	cout << "and multiplies them to get gross pay " << endl;
+	cout << "it then calculates net pay by subtracting 15%" << endl;
+	cout << "************************************************" << endl << endl;
+}
+
+//	*********************************************************************
+//	computePaycheck
+//
+//	task:	  This function takes rate and time and multiples them to
+//	          get gross pay and then finds net pay by subtracting 15%.
+//	data in:  pay rate and time in hours worked
+//	data out: the gross and net pay
+//
+//	********************************************************************
+
+void computePaycheck(float rate, int time, float gross, float net)
+{
+	// Fill in the code to find gross pay and net pay
+	gross = rate * time;
+    net = gross * .85;
+    cout << "Gross Pay: $" << gross << endl;
+	cout << "Net Pay: $" << net << endl;
+
+}
+
+
+
+/*
+Exercise 1: Fill in the code (places in bold) and note that the function
+computePaycheck determines the net pay by subtracting 15% from the gross
+pay. Both gross and net are returned to the main() function where those
+values are printed.
+
+Exercise 2: Compile and run your program with the following data and make
+sure you get the output shown.
+    Please input the pay per hour
+    9.50
+    Please input the number of hours worked
+    40
+
+    The gross pay is $380
+    The net pay is $323
+    We hoped you enjoyed this program
+
+Exercise 3: Are the parameters gross and net, in the modified calPaycheck
+function you created in Exercise 1 above, pass by value or pass by
+reference?
+    The parameters gross and net are pass by reference
+
+Exercise 4: Alter the program so that gross and net are printed in the function
+compute computePaycheck instead of in main(). The main() function executes
+the statement
+cout << "We hoped you enjoyed this program" << endl;
+after the return from the function calPaycheck.
+
+Exercise 5: Run the program again using the data from Exercise 2. You should
+get the same results. All parameters should now be passed by value.
+*/
---
+++ Lab 6.1B/swap.cpp
@@ -0,0 +1,73 @@
+/*
+Write a program that will read two floating point numbers (the first
+read into a variable called first and the second read into a variable called
+second) and then calls the function swap with the actual parameters first
+and second. The swap function having formal parameters number1 and
+number2 should swap the value of the two variables. Note: This is similar
+to a program you did in Lesson Set 1; however, now you are required to
+use a function. You may want to look at logicprob.cpp from Lesson Set 1.
+    Sample Run:
+    Enter the first number
+    Then hit enter
+    80
+    Enter the second number
+    Then hit enter
+    70
+    You input the numbers as 80 and 70.
+After swapping, the first number has the value of 70 which was the value of the second number
+The second number has the value of 80 which was the value of the first number
+*/
+
+//Hue Anh Huynh
+//3/12/17
+
+#include <iostream>
+
+using namespace std;
+
+void swap (float& num1, float& num2);
+
+int main(){
+
+    float n1;
+    float n2;
+
+
+    cout << "First Number: ";
+    cin >> n1;
+
+    cout << "Second Number: ";
+    cin >> n2;
+
+    cout << "You input the number as " << n1 << " and " << n2 << "." << endl;
+
+    swap (n1, n2);
+
+    cout << "After swapping, the first number has the value of " << n1 <<
+            " which was the value of the second number." << endl;
+    cout << "The second number has the value of " << n2 <<
+            " which was the value of the first number." << endl;
+
+    return 0;
+}
+
+void swap(float& num1, float& num2){
+
+    float t; //temp
+    t = num1;
+    num1 = num2;
+    num2 = t;
+
+}
+
+/*
+Exercise 1: Compile the program and correct it if necessary until you get no
+syntax errors.
+Exercise 2: Run the program with the sample data above and see if you get the
+same results.
+
+Exercise 3: The swap parameters must be passed by ____________.
+(Assume that main produces the output.) Why?
+    Pass by reference because the value in the actual parameter is changed.
+*/
+
