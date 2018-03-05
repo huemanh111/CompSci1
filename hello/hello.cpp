90a79060d4e2359ecf521c34b08dd4255c8b1223
Hue Anh <huynh.hueanh1@gmail.com>
2018-03-05 02:30:24 -0500

    Add files via upload

---
+++ hello/hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double n;
+    cout << "Hello world!\n";
+    cout << "The float is stored in " << sizeof(float)<< " bytes" << endl;
+    cout << "Variable amount is stored in " << sizeof(n) << " bytes" << endl;
+
+    return 0;
+}
