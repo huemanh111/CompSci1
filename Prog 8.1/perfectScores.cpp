640368f6a994dd4bb8dd2f9bff3ca6bbbb7d96cd
Hue Anh <huynh.hueanh1@gmail.com>
2018-03-30 00:10:40 -0400

    Add files via upload

---
+++ Prog 8.1/perfectScores.cpp
@@ -0,0 +1,49 @@
+/*Hue Anh Huynh
+  3/29/17
+
+Write a modular program that accepts at least 10 integer test scores
+from the user and stores them in an array. Then main should display
+how many perfect scores were entered (i.e., scores of 100).
+*/
+
+#include <iostream>
+
+using namespace std;
+
+const int TEST_SCORES = 10;
+int scores[TEST_SCORES];
+
+int main()
+{
+
+    int c = 0; //count of perfect scores
+    int test = 0;
+    int score;
+
+    cout << "Perfect Score Counter" << endl << endl;
+    cout << "Enter 10 test scores from 1 - 100: ";
+
+
+    for(int i = 0; i < TEST_SCORES; i++)
+    {
+        cin >> score;
+        while(score < 0 || score > 100)
+        {
+            cout << "Invalid input" << endl;
+            cin >> score;
+        }
+        scores[i] =  score;
+    }
+
+    for(int test = 0; test < TEST_SCORES ; test++)
+    {
+        if(scores[test] == 100)
+        {
+            c++;
+        }
+    }
+    cout << "Perfect Scores: " << c << endl;
+
+    return 0;
+}
+
---
+++ Prog 8.1/romanNumeralConverter.cpp
@@ -0,0 +1,32 @@
+/*Hue Anh Huynh
+  3/29/17
+Write a program that displays the roman numeral equivalent of any decimal number
+between 1 and 10 that the user enters. The roman numerals should be stored in an
+array of strings and the decimal number that the user enters should be used to locate
+the array element holding the roman numeral equivalent. The program should have a
+loop that allows the user to continue entering numbers until an end sentinel of 0 is
+entered.
+*/
+
+#include <iostream>
+using namespace std;
+
+string ROMAN_NUMERAL[10] = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+
+
+int main()
+{
+    int n;
+
+    cout << "ROMAN NUMERAL CONVERTER" << endl;
+    cout << "Enter a number from 1 to 10 too be converted: " ;
+    cin >> n;
+
+    while (n < 1 || n > 10)
+    {
+        cout << "The input is invalid. The number must be between 1 and 10. Please try again: ";
+        cin >> n;
+    }
+    cout << "Roman Numeral: " << ROMAN_NUMERAL[n - 1] << endl;
+    return 0;
+}
